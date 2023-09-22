4b5e0b0129aaed2eaf832f4ee258cdf2b12af64e
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-09-22 13:53:49 +0700

    ปรับ background

--- resources/views/frontend/shop.blade.php
+++ resources/views/frontend/shop.blade.php
@@ -5,12 +5,11 @@
 @endsection
 
 @section('content')
-
     <div class="py-5">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h1 class="mb-3">หมวดหมู่</h1>
+                    <h1 class="mb-3">หมวดหมู่ 55</h1>
                     <div class="row">
                         @foreach ($category as $item)
                             <div class="col-md-3 mb-3">
@@ -19,7 +18,10 @@
 
                                         <img src="{{ asset('assets/uploads/category/' . $item->image) }}"
                                             class="text-center fw-bold position-relative" alt="Category Image">
-                                        <h4 class="position-absolute top-50 start-50 translate-middle">{{ $item->name }}
+
+                                        <h4
+                                            class="position-absolute top-50 start-50 translate-middle background-text-image">
+                                            {{ $item->name }}
                                         </h4>
 
                                     </div>
